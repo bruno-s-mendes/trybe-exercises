91e612b5795605e0aa1f7e8228594d6a56d160dd
Bruno Mendes <mendessbruno@gmail.com>
2021-03-01 15:44:22 -0300

    conclusao do exercicio de aula 1 dia 4.4

--- exercises/4.0/4.3/exerciciosDoDia.js
+++ exercises/4.0/4.3/exerciciosDoDia.js
@@ -43,7 +43,7 @@ if (num>1) {
 }
 //exercicio 4
 
-let num = 3;
+let num = 6;
 let asterisco = '*';
 let out='';
 let space = ' ';
@@ -64,3 +64,23 @@ if (num>1) {
 
 // exercicio 5
 
+let num = 3;
+let asterisco = '*';
+let out= 0;
+let space = ' ';
+let maxSpace ='';
+let intSpace ='';
+
+if (num>1) {
+    for (let index = 1; index <= num; index +=1){
+        out += 1;
+            for (let indexSpace = 1; indexSpace <= (((num-index)/2)+((num-index)%2)); indexSpace +=1) {
+                maxSpace += space;
+            }
+             for () {
+                intSpace += space;
+            }
+            console.log (maxSpace + asterisco + out)
+            maxSpace ='';
+            }
+}
---
+++ exercises/4.0/4.4/exerciciosAula.js
@@ -0,0 +1,21 @@
+//EXERCICO 1
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+};
+
+let medals = {
+    golden: 2, 
+    silver: 3,
+};
+
+console.log ('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+
+player ['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
+
+console.log ('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
+
+console.log ('A jogadora ' + player.name + ' ' + player.lastName + ' possui ' + medals.golden + ' medalhas de ouro e ' + medals.silver + ' medalhas de prata.');
+
