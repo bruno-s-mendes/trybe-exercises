2defcf26a9de9955ca352c15c360fc816ba99ade
Bruno Mendes <mendessbruno@gmail.com>
2021-04-05 14:26:25 -0300

    resolvido ex 7

--- exercises/8.0/8.3/exercicio7.js
+++ exercises/8.0/8.3/exercicio7.js
@@ -66,7 +66,7 @@ const books = [
 const expectedResult = 'O Senhor dos Anéis';
 
 function authorWith3DotsOnName() {
-  // escreva seu código aqui
+  return books.find((book) => (book.author.name[1] === '.' && book.author.name[4] === '.' && book.author.name[7] === '.')).name;
 }
 
 assert.deepStrictEqual(authorWith3DotsOnName(), expectedResult);
