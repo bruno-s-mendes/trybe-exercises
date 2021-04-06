d3aa39e4b1800b8e2c7053653ced4208a769624a
Bruno Mendes <mendessbruno@gmail.com>
2021-04-06 18:41:45 -0300

    resolvido ex6, esperando a luz voltar para fazer push

--- exercises/8.0/8.4/exercise6.js
+++ exercises/8.0/8.4/exercise6.js
@@ -4,7 +4,13 @@ const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
-  // escreva seu código aqui
+  return students.map((student, index) => {
+     let obj = {
+          name: student,
+          average: grades[index].reduce((acc, grade) => acc += grade,0)/(grades[index].length)
+      }
+      return obj;
+  });
 }
 
 const expected = [
