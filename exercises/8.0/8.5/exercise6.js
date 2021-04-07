f2446b7cf49b6a425e97e7c6fc6c0003afe73665
Bruno Mendes <mendessbruno@gmail.com>
2021-04-07 18:33:03 -0300

    resolvido ex6 e copiado ex7

--- exercises/8.0/8.5/exercise6.js
+++ exercises/8.0/8.5/exercise6.js
@@ -6,6 +6,10 @@ const chiron = ['Chiron', 'Bugatti', 2016];
 
 // escreva toObject abaixo
 
+const toObject = ([name, brand, year]) => {
+    return {name, brand, year}
+};
+
 assert.deepStrictEqual(toObject(palio), { name: 'Palio', brand: 'Fiat', year: 2019 });
 assert.deepStrictEqual(toObject(shelbyCobra), { name: 'Shelby Cobra', brand: 'Ford', year: 1963 });
 assert.deepStrictEqual(toObject(chiron), { name: 'Chiron', brand: 'Bugatti', year: 2016 });
