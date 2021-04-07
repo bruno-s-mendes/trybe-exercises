a73812d505fe413afd7e49d8aaabe171721b4b0f
Bruno Mendes <mendessbruno@gmail.com>
2021-04-07 15:57:16 -0300

    resolvido ex2 e copiado ex3

--- exercises/8.0/8.5/exercise2.js
+++ exercises/8.0/8.5/exercise2.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-// escreva sum abaixo
+const sum = (...num) => num.reduce((acc, number) => acc + number, 0);
 
 assert.strictEqual(sum(), 0);
 assert.strictEqual(sum(1), 1);
